d200c794b132d17cf74447bfd13d10787e801ac2
Timberzzman <leloupandco@gmail.com>
2021-12-10 06:53:46 +0100

    Deleting husky from server package.json and trying to log event in client apiWorker.js

--- client/src/apiWorker.js
+++ client/src/apiWorker.js
@@ -1,9 +1,15 @@
 export default async function apiWorker(path, options) {
   console.log(options);
   const { API_URL } = process.env;
-  const response = await fetch(`${API_URL}/${path}`);
-  const data = await response.json();
+  console.log(`${API_URL}${path}`);
+  try {
+    const response = await fetch(`${API_URL}${path}`);
+    const data = await response.json();
 
-  console.log(data);
-  return data;
+    console.log(data);
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+  return null;
 }
